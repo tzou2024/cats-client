02c85414801132b69bb9de15e9f4a2faaee922d6
Trevor Zou <tzou@olin.edu>
2022-08-10 22:46:56 -0500

    remove slash from apiconfig endpoint

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://catsserv.herokuapp.com/',
+	production: 'https://catsserv.herokuapp.com',
 	development: 'http://localhost:8000',
 }
 
